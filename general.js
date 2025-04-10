d1b25189817592bd40c395f113d16ac41059a27d
SariCohen2 <s0556774746r@gmail.com>
2025-04-11 00:49:44 +0300

    add server and suitable UI

---
+++ general.js
@@ -0,0 +1,286 @@
+const songs = [
+    {
+        title: "וְאֵיךְ/ @CE3D",
+        audioSrc: "***",
+        lyrics: `צִוִּיתָ
+לֹא תִקַּח הָאֵם עַל הַבָּנִים
+וְאֵיךְ את הָאֵם עִם הַבָּנִים
+פִּרְאֵי אָדָם בַּשֶּׁבִי לָקָחוּ
+
+אָמַרְתָּ
+עֲשֵׂה כָּל אֲשֶׁר תֹּאמַר שָׂרָה
+ואֵיךְ עָלְתָה שִׁירִי בִּסְעָרָה
+הַכְּלָבִים אֶת כְּפִירֶיהָ סָחֲבוּ
+
+הִבְטַחְתָּ
+אֵשׁ תָּמִיד תּוּקַד עַל הַרְאֵל
+וְאֵיךְ נֶעֶקְדוּ כְּפִיר וַאֲרִיאֵל
+דָּמִים בְּדָמִים נָגָעוּ
+
+בִּקַּשְׁתָּ
+עַל אֵם הַדֶּרֶךְ מִנְעִי קוֹלֵךְ מִבֶּכִי
+וְאֵיךְ תּוּכְלִי לְהַפְסִיק? אָנָּא הַמְשִׁיכִי
+לִשְׁאֹג כִּלְבִיאָה עַל כִּי לֹא שָׁבוּ
+
+קָרָאתָ
+הֲבֵן יַקִּיר לִי... יֶלֶד שַׁעֲשׁוּעִים
+וְאֵיךְ צָלְחָה דֶּרֶךְ רְשָׁעִים
+וּמִשַּׁאֲגוֹת הַלַּיִשׁ לֹא נִבְהָלוּ
+
+הִבְטַחְתָּ
+דַּי דַּי, כִּי יֵשׁ שָׂכָר לִפְעֻלָּתֵךְ
+וְאֵיךְ מִבּוֹר שְׁבִי תֵּדְעִי אַחֲרִיתֵךְ
+למנוחות עולמים איתך שָׁבוּ
+
+נִבֵּאתָ
+"חֲוִי כִּמְעַט רֶגַע" כּוֹס הַיְּגוֹנִים הַרְפִּי
+וְאֵיךְ הֶאֱמַנְתִּי כִּי אֲדַבֵּר? אף על פי
+אֵם הַבָּנִים שְׂמֵחָה
+
+
+ואף על פי שיתמהמה אחכה לו
+בכל יום שיבוא
+אני מאמין`,
+        showDetails: false
+    },
+    {
+        title: "הַהֹפְכִי הַצּוּר אֲגַם מָיִם/ @CE3D",
+        audioSrc: "songs/libenu_itchem.mp3",
+        lyrics: `
+בְּתִמְּרוֹת עָשָׁן נֶחְטַפְתִּי
+לְנִסָּיוֹן הַשְּׁבִי נִבְחַרְתִּי
+בְּמִנְהֲרוֹת חַמַאס יָשַׁבְתִּי
+אַךְ בְּרוּחִי לֹא נָפַלְתִּי
+
+בְּעַזּוּת דִּקְדוּשָׁה שַׁבָּת שָׁמַרְתִּי
+בְּמַאֲכָלוֹת אֲסוּרִים לֹא נִגְעַלְתִּי
+כּוֹס תַּרְעֵלָה עַד תֹּם שָׁתִיתִי
+אך, עַל עִקְּרֵי הַדָּת עָמַדְתִּי
+
+בְּעַזָּה לֹא כֻּבְּתָה שַׁלְהַבְתִּי
+בְּאֵשׁ עַזָּה בָּעֲרָה גַּחַלְתִּי
+בְּבֵית ה' בעז"ה, יִהְיֶה שִׁבְתִּי
+כִּי עַזָּה כְּמָוֶת אָהַבְתִּי
+
+ה' לְנֶגְדִּי תָּמִיד שיויתִי
+לַאֲגַם יְשׁוּעוֹת קִוִּיתִי
+אֶת חַבְרוֹתַי עוֹדַדְתִּי
+וַיִּשְׁמַע ה' זַעֲקָתִי וְאֶנְקָתִי
+
+לְפֶתַע בַּת קוֹל שָׁמַעְתִּי
+קוּמִי לָךְ יָפָתִי תַּמָּתִי
+בְּקוֹל דְּמָמָה עָנִיתִי:
+בְּצֵל כְּנָפֶיךָ חָסִיתִי
+
+בַּחֹשֶׁךְ, אוֹר גָּדוֹל רָאִיתִי
+עַל בַּעַל הַבִּירָה, הֵצַצְתִּי *
+לְקֵץ תפ"ב יָמִים נוֹשַׁעְתִּי
+מִמְּאוּרוֹת אֹפֶל נִפְדְּתִּי
+
+בְּדֶרֶךְ אֱמוּנָה בָּחַרְתִּי
+וּבְדֶרֶךְ אֱמוּנָה שַׁבְתִּי
+וְעָנְתָה אִמִּי הוֹרָתִי:
+אִם שְׁתַּיִם הֵן אֲנִי וּבִתִּי **
+
+*רבי יצחק משל לאחד שהיה עובר ממקום למקום וראה בירה אחת דולקת אמר תאמר שהבירה זו בלא מנהיג הציץ עליו בעל הבירה אמר לו אני הוא בעל הבירה כך לפי שהיה אבינו אברהם אומר תאמר שהעולם הזה בלא מנהיג הציץ עליו הקב"ה ואמר לו אני הוא בעל העולם (בראשית רבה ל"ט א')
+**א"ר ירמיה משום רשב"י ראיתי בני עלייה והן מועטין אם אלף הן אני ובני מהן אם מאה הם אני ובני מהן אם שנים הן אני ובני הן (סוכה מ"ה ב')`,
+        showDetails: false
+    },
+    {
+        title: 'חמש מאות ימים/ @מתלמידי הגר"א',
+        audioSrc: 'songs/hamishamim.mp3',
+        lyrics: `בֵּית א:
+חָמֵשׁ מֵאוֹת יָמִים
+אֲנַחְנוּ מְחַכִּים,
+בְּלֵילוֹת בּוֹכִים, מְחַכִּים כְּבָר לְעִסְקָה
+יוֹצֵאת הַצְהָרָה
+אוּלַי יֵשׁ תִּקְוָה
+וְאָז בַּמִּבְצָע, מַחֲזִירִים אֶת הַגּוּפָה
+פִּזְמוֹן:
+בְּלֵילוֹת בִּכְיָה, בּוֹקַעַת לַשָּׁמַיִם
+וְאָנוּ כְּבָר בַּאֲפִיסַת כּוֹחוֹת
+יֵשׁ פִּתְאוֹם מִמְצָא חָדָשׁ
+מֵפִיחַ בִּי אֶת הַחַיִּים
+וְאָז אֲנִי שׁוּב מְקַוֶּה לְהַצְלָחוֹת
+בַּיִת ב:
+נָשִׂיא חָדָשׁ נִכְנָס
+וּקְצָת נִכְנָע חַמַאס
+וּלְפֶתַע מוֹדִיעִים
+שֶׁתִּהְיֶה עִסְקָה
+חֶלְקָם מִשְׁתַּחְרְרִים
+הַשְּׁאָר אִתָּם בּוֹכִים
+אֲבָל מִתּוֹךְ הַלֵּב
+יוֹצֵאת וְעוֹלֶה
+פִּזְמוֹן:
+בַּלֵּילוֹת בִּכְיָה וְכוּ'
+`,
+        showDetails: false
+    },
+    {
+        title: 'ילדים הם לא משחק/ @אוראל סולטן',
+        audioSrc: 'songs/imtashalu.mp3',
+        lyrics: `מילים מתנפצות על החול
+חרוטות בדם
+רגליים יחפות על חוף רועד
+שקיעות טובעות אל פני הים
+
+לבבות קופאים בשמש
+דממות זולגות בדמעה
+וחורקת קרן גשם
+ותריס נסגר עם סחרור חמה
+
+אמא בישלה שתיקה
+באש שמש כבויה
+שרפה דלת חרוקה
+שבמסתרי הנפש חבויה
+
+ילדים הם לא משחק
+רועד על אבן גיר
+שלושה כתמים שוקעים
+תפוזים ללא מחיר
+
+פעם היו לי אנשים
+היום נותרו רק השמות
+ואין פנים לאופק
+ועלה על גדותיו ישימון
+
+אם תהיה לי זכות
+יום אחד שאלה לתחום
+אפתח את פי בהשתפכות
+ואומר לצבא מרום
+
+אם יש כאן משהו
+למה אין בי כלום
+אם אין טובע בשפיות
+למה חותרים לדמדום
+
+אם אין כאן דם
+למה הוא עוד מתבוסס בעצמו
+
+אם יש כאן ים
+למה אני רואה רק
+מוות
+`   ,
+        showDetails: false
+    },
+    {
+        title: 'רק שתחזרו/ @מהללו'
+        , audioSrc: './audio/zz_gen1.mp3',
+        lyrics: `
+בית 1
+We miss them.
+We think about them.
+We worry about them.
+We pray for them.
+
+פזמון
+May Hashem bring them back home
+Bring them back home
+Bring them back home
+עוד היום.
+
+בית 2
+We want to see them here.
+we want to hear them, to hear.
+We want to feel them close.
+We want to watch them grow.
+
+גשר
+Loneliness, darkness, sadness, thirst, guilt, fear, pain, hunger, worry, slavery
+
+
+בית 1
+אנחנו מתגעגעים אליהם.
+אנחנו חושבים עליהם.
+אנחנו דואגים להם.
+אנחנו מתפללים עבורם.
+
+פזמון
+מי יתן וה' יחזיר אותם חזרה הביתה.
+...
+עוד היום.
+
+בית 2
+אנחנו רוצים לראות אותם כאן.
+אנחנו רוצים לשמוע אותם.
+אנחנו רוצים להרגיש אותם קרובים.
+אנחנו רוצים לראות אותם גדלים.
+
+גשר
+בדידות, חושך, עצב, צמא, אשמה, פחד, כאב, רעב, דאגה, עבדות.`,
+        showDetails: false
+    },
+];
+
+// const container = document.getElementById("songsContainer");
+// container.className = "modal-content"
+
+// songs.forEach(song => {
+//     const songDiv = document.createElement("div");
+//     songDiv.className = "song";
+//     songDiv.dir='rtl'
+
+//     const title = document.createElement("h2");
+//     title.textContent = song.title;
+
+//     const audio = document.createElement("audio");
+//     audio.controls = true;
+//     const source = document.createElement("source");
+//     source.src = song.audioSrc;
+//     source.type = "audio/mpeg";
+//     audio.appendChild(source);
+
+//     const lyrics = document.createElement("div");
+//     lyrics.className = "lyrics";
+//     lyrics.id = id = "modalText"
+//     lyrics.textContent = song.lyrics;
+
+//     songDiv.appendChild(title);
+//     songDiv.appendChild(lyrics);
+//     songDiv.appendChild(audio);
+
+//     container.appendChild(songDiv);
+// });
+
+// פונקציה שתדאג להציג את רשימת השירים
+function renderSongList() {
+    const songsListDiv = document.getElementById('songsContainer');
+    songsListDiv.innerHTML = ''; // ננקה את התוכן הקודם
+    songsListDiv.className = "song";
+
+    songs.forEach((song, index) => {
+        const songTitle = document.createElement('h2');
+        songTitle.innerText = song.title + " ▼"
+        songTitle.dir = 'rtl'
+
+
+        // הוספת איוונט שמבצע שינוי ב-showDetails
+        songTitle.addEventListener('click', () => {
+            song.showDetails = !song.showDetails;
+            renderSongList(); // עדכון הרשימה כדי להראות את התוכן של השיר אם צריך
+        });
+
+        songsListDiv.appendChild(songTitle);
+
+        // אם showDetails הוא true, נציג את התוכן
+        if (song.showDetails) {
+            songTitle.innerText=songTitle.innerHTML.substring(0, songTitle.innerHTML.length - 2);
+            songTitle.innerHTML+="▲"
+            const audio = document.createElement('audio');
+            audio.controls = true;
+            const source = document.createElement('source');
+            source.src = song.audioUrl;
+            source.type = 'audio/mp3';
+            audio.appendChild(source);
+
+            const lyrics = document.createElement('p');
+            lyrics.innerText = song.lyrics;
+            lyrics.id = id = "modalText"
+            songsListDiv.appendChild(audio);
+            songsListDiv.appendChild(lyrics);
+        }
+    });
+}
+
+// קריאה לפונקציה כדי להציג את הרשימה
+renderSongList();
---
+++ scriptDetails.js
@@ -0,0 +1,228 @@
+// const basicUrl = "http://localhost:3000";
+const basicUrl = "https://hostagesserver.onrender.com";
+var hostage
+window.onload = () => {
+    openDetails()
+}
+function openDetails() {
+    // localStorage.setItem('hos',JSON.stringify(hostage))
+    // window.location.href="./all.html";
+    hostage = JSON.parse(localStorage.getItem("hos"))
+    text = hostage.lyrics.replace(/__OPEN__/g, '[').replace(/__CLOSE__/g, ']');
+    document.getElementById("modalImage").src = hostage.imageLink
+    document.getElementById('modalTitle').innerText = hostage.hostageName;
+    document.getElementById('likedArea').innerHTML += `<div><i class="fas fa-thumbs-up"  id="liked" onclick="likeOrDis(${hostage._id})"></i></div><div id="count">${hostage.likesCount} אנשים אהבו</div>`;
+
+    document.getElementById('description').innerText = hostage.description;
+    document.getElementById("mailto").href = `mailto:?subject=הי, רציתי לשתף אותך באתר שגיליתי- הקדשת שיר לכל חטוף?body=${encodeURIComponent('אתר להעלאת המודעות לחטופים ע"י כתיבת שירים וביצועם \n https://songs-of-hope.github.io/Home/ \n האתר כולו נבנה ע"י קהילת prog.co.il , כולל כתיבת השירים וביצועם ')}`
+    if (hostage.returned == true && hostage.returned2 == false) {
+        var parts = hostage.hostageName.split(" ");
+        if (parts.length > 1) {
+            // var lastName = parts[1];
+            var lastName = parts[1].substring(1);
+            // return firstName + " " + newLastName;
+            // console.log(lastName);
+            if (hostage.returned2 != true && hostage.died)
+                document.getElementById('description').innerText += `\n 💔${lastName}   הוחזר בתאריך ${hostage.returnDate}💔`
+            else if (hostage.died)
+                document.getElementById('description').innerText += `\n 💔${lastName}   הוחזרו בתאריך ${hostage.returnDate}💔`
+            else
+                document.getElementById('description').innerText += `\n 💛${lastName}   חזר בתאריך ${hostage.returnDate}💛`
+            document.getElementById("modalImage").className += 'returnImg'
+            document.getElementById('modalTitle').innerText = hostage.hostageName + " , " + lastName + " חזר הביתה";
+
+        }
+
+    }
+    else if (hostage.returned == true) {
+        document.getElementById("modalImage").className += 'returnImg'
+        if (hostage.died) {
+            const messages = [
+                hostage.hostageName + ", הלב שבור 💔",
+                hostage.hostageName + ", חסרים לנו כל כך 😢",
+                hostage.hostageName + ", תמיד בכאב 🖤",
+                hostage.hostageName + ", כל כך כואב לנו 😔",
+                hostage.hostageName + ", נשמות טהורות, לא נשכח 🕊️",
+                hostage.hostageName + ", געגועים אינסופיים 😞",
+                hostage.hostageName + ", לא מצליחים להפסיק לבכות 😭",
+                hostage.hostageName + ", כל כך קשה להאמין 💔"
+            ];
+            document.getElementById('modalTitle').innerText = messages[Math.floor(Math.random() * messages.length)];
+            if (!hostage.returned2)
+                document.getElementById('description').innerText += `\n 💔${hostage.hostageName}   הוחזר בתאריך ${hostage.returnDate}💔`
+            else
+                document.getElementById('description').innerText += `\n 💔${hostage.hostageName}   הוחזרו בתאריך ${hostage.returnDate}💔`
+
+        }
+        else {
+            document.getElementById('description').innerText += `\n💛 חזר בתאריך ${hostage.returnDate}💛`
+            document.getElementById('modalTitle').innerText = hostage.hostageName + ", חזר הביתה";
+
+        }
+
+    }
+    document.getElementById('btn').onclick = () => {
+        copySong(text)
+    };
+    document.getElementById('modalText').innerHTML = text.replace(/\n/g, '<br>');
+    if (hostage.audioLink === '***') {
+        document.getElementById('modalAudio').style.opacity = '0';
+        document.getElementById('notExist').innerHTML = 'השיר לא נוצר עדיין בmp3. אולי את/ה תתנדב/י ליצור אותו?'
+
+    }
+    else
+        document.getElementById('modalAudio').src = hostage.audioLink;
+
+    // if (hostage.returned)
+    //     document.getElementById('modalTitle').innerText = hostage.hostageName + ", חזר הביתה";
+
+    var downloadBtn = document.getElementById('downloadBtn');
+    downloadBtn.addEventListener('click', function () {
+        if (hostage.audioLink) {
+            var link = document.createElement('a');
+            link.href = hostage.audioLink;
+            link.download = hostage.name + '.mp3';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            alert("אין קובץ להורדה.");
+        }
+    });
+    document.getElementById('detailsModal').style.display = 'block';
+    displayComments()
+
+}
+function closeModal() {
+    window.location.href = "./index.html";
+
+    // document.getElementById('modalAudio').style.opacity = '1';
+    // document.getElementById("modalImage").classList.remove("returnImg");
+    // document.getElementById('notExist').innerHTML = ''
+    // document.getElementById('detailsModal').style.display = 'none';
+    // document.getElementById('modalAudio').pause();
+    // document.getElementById('modalAudio').currentTime = 0;
+}
+
+
+async function likeOrDis(objId) {
+    const response = await fetch(`${basicUrl}/api/documents/${objId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ objId: objId })
+    });
+    const data = await response.json();
+
+    console.log(data);
+    document.getElementById('count').innerHTML = `${data.doc.likesCount} אנשים אהבו`
+    localStorage.setItem('hos', JSON.stringify(data.doc))
+    // let message=
+    if (!data.ok)
+        showNotification('הסרת את הלייק!')
+    else
+        showNotification('תודה על התגובה🙏')
+
+    // alert(`Help request status updated to ${helpRequest.status}\n"your Id for next time is:"${helpRequest._id}`);
+}
+
+function copySong() {
+    const textToCopy = document.getElementById('modalText').innerText;
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+            const btn = document.getElementById("btn");
+            const originalText = btn.innerHTML;
+            btn.innerHTML = "!!הועתק בהצלחה";
+
+            setTimeout(() => {
+                btn.innerHTML = originalText;
+            }, 2000);
+            // document.getElementById("btn").innerHTML+=""
+            // alert("מילות השיר הועתקו בהצלחה!");
+        })
+        .catch(err => {
+            console.error('שגיאה בהעתקת הטקסט: ', err);
+        });
+}
+
+async function send(tag) {
+    let sended = JSON.parse(localStorage.getItem(`hos${hostage._id}`))
+    console.log(sended);
+
+    if (!sended || sended.id != hostage._id) {
+        // console.log('sended._id->'+sended.id,' hostage._id->',hostage._id);
+
+        let objForSave = {
+            id: hostage._id,
+            tag: tag
+        }
+        localStorage.setItem(`hos${hostage._id}`, JSON.stringify(objForSave))
+        hostage.tags.push(tag)
+    }
+
+    else {
+        console.log('sended==', sended.tag);
+        let index = hostage.tags.indexOf(sended.tag)
+        if (index >= 0) {
+            hostage.tags[index] = tag
+            // localStorage.setItem('afc', tag)
+            let objForSave = {
+                id: hostage._id,
+                tag: tag
+            }
+            localStorage.setItem(`hos${hostage._id}`, JSON.stringify(objForSave))
+        }
+        else
+            return
+        console.log(' כבר הגבת');
+    }
+    const response = await fetch(`${basicUrl}/api/documents/edit/${hostage._id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(hostage.tags)
+    });
+    const data = await response.json();
+    console.log(data);
+    localStorage.setItem('hos', JSON.stringify(data))
+
+
+    displayComments()
+    // console.log(data);
+    // localStorage.setItem('hos',JSON.stringify(data.doc))
+
+}
+function displayComments() {
+    const commentsElement = document.getElementById('comments');
+    commentsElement.innerHTML = ''
+    let comments = hostage.tags
+
+    comments.forEach((comment, index) => {
+        if (index < 5) {
+            const commentElement = document.createElement('span');
+            commentElement.dir='rtl'
+            commentElement.className = 'comment'
+            commentElement.innerHTML = comment;
+            commentsElement.appendChild(commentElement);
+        }
+    })
+    if (comments.length > 5)
+        commentsElement.innerHTML += `<span class="loadMore">+ ${comments.length-5}</span>`
+    // comments.innerHTML = '';
+}
+function showNotification(message) {
+
+    const container = document.getElementById("alert");
+    const notificationContainer = document.createElement("div");
+    notificationContainer.textContent = message;
+    notificationContainer.className = "notification";
+
+    container.appendChild(notificationContainer);
+
+    setTimeout(() => {
+        notificationContainer.classList.add("show");
+    }, 100); //Adding a small delay to create an animation
+
+    setTimeout(() => {
+        notificationContainer.classList.remove("show");
+        setTimeout(() => container.removeChild(notificationContainer), 300);
+    }, 1000);
+}
